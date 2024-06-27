20011e1213fc7e961153a9ff89b1bb4595dc10cb
Kayky Oliveira <kaykycf1245@gmail.com>
2024-06-27 16:51:05 -0300

    Exception handling

---
+++ dscommerce/src/main/java/com/dev/superior/dscommerce/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.dev.superior.dscommerce.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+}
